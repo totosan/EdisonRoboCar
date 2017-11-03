298cbb74bc6a87fb5e0a08a5d67730ae22af32ff
To To <toto_san@live.com>
2017-11-03 11:49:22 +0100

    added first version

---
+++ Motor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using MraaSharp;
+
+namespace RoboCar
+{
+	public class Motor
+	{
+		Pwm speed;
+		Gpio direction;
+		Gpio brake;
+
+		public enum Direction
+		{
+			Forward=1,
+			Backward=0
+		}
+
+		public Motor(int pwmPin, int directionPin, int brakePin)
+		{
+			speed = new Pwm(pwmPin);
+			direction = new Gpio(directionPin, MraaGpioDir.Out);
+			brake = new Gpio(brakePin, MraaGpioDir.Out);
+
+			InitializeMotor();
+		}
+
+		private void InitializeMotor()
+		{
+			direction.Write(MraaGpioValue.High);
+			brake.Write(MraaGpioValue.Low);
+		}
+
+		public void SetDirection(Direction dir)
+		{
+			if (dir == Direction.Forward)
+				direction.Write(MraaGpioValue.High);
+			else
+				direction.Write(MraaGpioValue.Low);
+		}
+
+		public void Brake()
+		{
+			brake.Write(MraaGpioValue.High);
+		}
+
+		public void Release()
+		{
+			brake.Write(MraaGpioValue.Low);
+		}
+
+		public void StartEngine(float velocity = 0.0f)
+		{
+			speed.Enable = true;
+			speed.Write(velocity);
+			Release();
+		}
+
+		public void StopEngine()
+		{
+			Brake();
+			speed.Write(0.0f);
+			speed.Enable = false;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using MraaSharp;
+
+namespace RoboCar
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Mraa.Initialize();
+			Console.WriteLine("Version: {0}", Mraa.Version);
+			Console.WriteLine("PlatformName: {0}", Mraa.PlatformName);
+
+			Motor engine = new Motor(3, 12, 9);
+			Motor gear = new Motor(11, 13, 8);
+
+			engine.StartEngine(0.5f);
+			Console.WriteLine("Stared engine!");
+			//gear.StartEngine(0.3f);
+			//Thread.Sleep(100);
+			//gear.Brake();
+
+			Console.ReadLine();
+			//while (true)
+			//{
+			//	Thread.Sleep(1000);
+			//	Console.WriteLine(a0.Read());
+			//}
+
+			engine.StopEngine();
+		}
+	}
+}
